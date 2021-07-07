5549db6ad247dd473fa20ea991367fccc249d0e5
mlgmontage <kettleofrassel1@gmail.com>
2021-07-07 16:07:40 +0500

    extending webform

--- website_hr_recruitment_form_extention/models/models.py
+++ website_hr_recruitment_form_extention/models/models.py
@@ -1,40 +1,29 @@
 from odoo import models, fields, api
-# partner_name
-# email_from
-# partner_phone
-# job_id [hidden]
-# department_id [hidden]
-
-# # Extension
-# Photo
-# gender (Selection)
-# Nationality
-# DateOfBirth
-# HomeAddress
-# Citizenship
-# MaritalStatus
-# languages
-# additionalCourses
-# Workplace
-# Position
-# StartDate
-# EndDate
-# StillWorking
-
-# University
-# StartDate
-# EndDate
-
-# isExperienced
-# Skills
 
 
 class ApplicantExtension(models.Model):
     _inherit = 'hr.applicant'
 
-    nationality = fields.Char(string="Nationality")
-    skills = fields.Text(string="Skills")
     gender = fields.Char(string="Gender")
+    photo = fields.Image(string="Profile Photo")
+
+    nationality = fields.Char(string="Nationality")
+    dateOfBirth = fields.Date(string="Date of Brith")
     homeAddress = fields.Char(string="Home Address")
     citizenship = fields.Char(string="Citizenship")
     maritalStatus = fields.Char(string="Marital Status")
+    languages = fields.Text(string="Languages")
+    additionalCourses = fields.Text(string="Additional Courses")
+    organization = fields.Char(string="Organization")
+
+    hasExperience = fields.Boolean(string="Has work experience?")
+    position = fields.Char(string="Postition")
+    workStart = fields.Date(string="Work start date")
+    workEnd = fields.Date(string="Work end date")
+    stillWorking = fields.Boolean("Still working?")
+
+    university = fields.Char(string="University")
+    specialization = fields.Char(string="Specialization")
+    universityStart = fields.Date(string="University start date")
+    universityEnd = fields.Date(string="University end date")
+    skills = fields.Text(string="Skills")
